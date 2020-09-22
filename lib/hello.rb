cfb9ebd21c8390b2203c975b16c8e58de65f3123
Viviana Lugo <vivi28lu@gmail.com>
2020-09-22 03:16:11 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+<<<<<<< HEAD
   if block_given?
     i = 0 
 
@@ -17,4 +18,13 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
+=======
+  i = 0 
+
+  while i < array.length 
+    yield array[i]
+    i = i + 1 
+  end
+  array
+>>>>>>> 3e068ef0e6bd5af121b0e46a84f3e72338f79d67
 end
